0c5a29553154989388723e19eee58b577e210074
Esa Rijal Mustaqbal <Esa_RM963@mitrais.com>
2018-10-30 10:01:53 +0700

    add local ui combine with redux

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -2,10 +2,38 @@ import React from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            age: ''
+        }
+    }
 
-export default addPerson;
+    nameChangedHandler = (event) => {
+        this.setState({name: event.target.value});
+    }
+
+    ageChangedHandler = (event) => {
+        this.setState({age: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="AddPerson">
+                <input type="text" 
+                        placeholder="name"
+                        onChange={this.nameChangedHandler}
+                        value={this.state.name}></input><br/>
+                <input type="number" 
+                        placeholder="age"
+                        onChange={this.ageChangedHandler}
+                        value={this.state.age}></input><br/>
+                <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+            </div>
+        )
+    }
+}
+
+export default AddPerson;
